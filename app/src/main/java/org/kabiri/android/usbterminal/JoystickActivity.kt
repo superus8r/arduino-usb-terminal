930b47822bd27450e4d2c6ef60b9ad066d9ddf41
Ali Kabiri <ali@kabiri.org>
2020-04-12 17:12:14 +0200

    extract content description strings for joystick buttons

--- app/src/main/java/org/kabiri/android/usbterminal/JoystickActivity.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/JoystickActivity.kt
@@ -8,5 +8,6 @@ class JoystickActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joystick)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
