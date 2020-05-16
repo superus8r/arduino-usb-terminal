30dec4799b983c69d3ccd71179cfe07a0d102193
Ali Kabiri <ali@kabiri.org>
2020-05-16 09:43:30 +0200

    add service name helper to the app module to make it injectable using Koin

--- app/src/main/java/org/kabiri/android/usbterminal/koin/AppModule.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/koin/AppModule.kt
@@ -3,6 +3,7 @@ package org.kabiri.android.usbterminal.koin
 import org.kabiri.android.usbterminal.arduino.ArduinoHelper
 import org.kabiri.android.usbterminal.arduino.ArduinoPermissionBroadcastReceiver
 import org.kabiri.android.usbterminal.arduino.ArduinoSerialReceiver
+import org.kabiri.android.usbterminal.data.ServiceNameHelper
 import org.kabiri.android.usbterminal.viewmodel.MainActivityViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -15,6 +16,7 @@ val appModule = module {
     single { ArduinoPermissionBroadcastReceiver() }
     single { ArduinoSerialReceiver() }
     single { ArduinoHelper(get(), get(), get()) }
+    single { ServiceNameHelper(get()) }
 
     viewModel { MainActivityViewModel(get()) }
 }
