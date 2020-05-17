9a0f66d20a8841866cb76f3b1c9db33e350b39dd
Ali Kabiri <ali@kabiri.org>
2020-05-17 14:15:06 +0200

    cleanup the SettingsActivity to have two new settings for setting the server name and selecting another device as the server

--- app/src/main/java/org/kabiri/android/usbterminal/SettingsActivity.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/SettingsActivity.kt
@@ -2,7 +2,9 @@ package org.kabiri.android.usbterminal
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import org.kabiri.android.usbterminal.data.SettingsReader
 import org.kabiri.android.usbterminal.network.wifi.NsdHelper
@@ -40,6 +42,21 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val selectedServerPreference: ListPreference =
+                findPreference(resources.getString(R.string.settings_key_selected_server))
+                    ?: return
+            updateDiscoveryData(selectedServerPreference)
+        }
+
+        private fun updateDiscoveryData(lp: ListPreference) {
+            val entries = arrayOf("Rabbit's Server", "Cat's Server")
+            val entryValues = arrayOf("server_rabbit", "server_cat")
+            lp.entries = entries
+            lp.entryValues = entryValues
         }
     }
 }
