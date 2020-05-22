13451dcf4f19b6b4631ca695e8071186019e80e9
Ali Kabiri <ali@kabiri.org>
2020-05-22 20:52:13 +0200

    remove settings reader provider

--- app/src/main/java/org/kabiri/android/usbterminal/koin/AppModule.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/koin/AppModule.kt
@@ -4,6 +4,7 @@ import org.kabiri.android.usbterminal.arduino.ArduinoHelper
 import org.kabiri.android.usbterminal.arduino.ArduinoPermissionBroadcastReceiver
 import org.kabiri.android.usbterminal.arduino.ArduinoSerialReceiver
 import org.kabiri.android.usbterminal.data.ServiceNameHelper
+import org.kabiri.android.usbterminal.data.SettingsReader
 import org.kabiri.android.usbterminal.viewmodel.MainActivityViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
