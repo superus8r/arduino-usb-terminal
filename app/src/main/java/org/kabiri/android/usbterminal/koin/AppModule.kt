22044e78bc7073a29c9bdaedb05ab185f6a40a55
Ali Kabiri <ali@kabiri.org>
2020-05-23 14:20:41 +0200

    update the AppModule for Koin to provide the AppDatabase, WifiDeviceRepository, and WifiDeviceListViewModel

--- app/src/main/java/org/kabiri/android/usbterminal/koin/AppModule.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/koin/AppModule.kt
@@ -1,11 +1,14 @@
 package org.kabiri.android.usbterminal.koin
 
+import androidx.lifecycle.SavedStateHandle
 import org.kabiri.android.usbterminal.arduino.ArduinoHelper
 import org.kabiri.android.usbterminal.arduino.ArduinoPermissionBroadcastReceiver
 import org.kabiri.android.usbterminal.arduino.ArduinoSerialReceiver
+import org.kabiri.android.usbterminal.data.AppDataBase
 import org.kabiri.android.usbterminal.data.ServiceNameHelper
-import org.kabiri.android.usbterminal.data.SettingsReader
+import org.kabiri.android.usbterminal.data.WifiDeviceRepository
 import org.kabiri.android.usbterminal.viewmodel.MainActivityViewModel
+import org.kabiri.android.usbterminal.viewmodel.WifiDeviceListViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -18,6 +21,9 @@ val appModule = module {
     single { ArduinoSerialReceiver() }
     single { ArduinoHelper(get(), get(), get()) }
     single { ServiceNameHelper(get()) }
+    single { AppDataBase.getInstance(get()) }
+    single { WifiDeviceRepository.getInstance((get() as AppDataBase).wifiDeviceDao()) }
 
     viewModel { MainActivityViewModel(get()) }
+    viewModel { (handle: SavedStateHandle) -> WifiDeviceListViewModel(handle, get()) }
 }
