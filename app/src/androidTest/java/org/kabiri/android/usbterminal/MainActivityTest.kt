1873172111c156672b81d7fc8b41d869fff500c0
Ali Kabiri <k4biri@icloud.com>
2022-10-06 22:00:14 +0200

    update fastlane; update tests

--- app/src/androidTest/java/org/kabiri/android/usbterminal/MainActivityTest.kt
+++ app/src/androidTest/java/org/kabiri/android/usbterminal/MainActivityTest.kt
@@ -5,14 +5,18 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
     @get:Rule
-    var rule = ActivityScenarioRule(MainActivity::class.java)
+    var rule = activityScenarioRule<MainActivity>()
 
     @Test
     fun checkUiViewsAreDisplayed() {
