9d01a3dcd390dc98e4f94bde6c3ac384d81aa037
Ali Kabiri <k4biri@icloud.com>
2024-03-30 19:28:34 +0100

    feature: annouce saved state after user changes a setting value

--- app/src/main/java/org/kabiri/android/usbterminal/ui/setting/SettingValueItem.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/ui/setting/SettingValueItem.kt
@@ -15,17 +15,38 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.kabiri.android.usbterminal.R
 import org.kabiri.android.usbterminal.ui.theme.UsbTerminalTheme
+import org.kabiri.android.usbterminal.ui.setting.SaveState.DEFAULT
+import org.kabiri.android.usbterminal.ui.setting.SaveState.UNSAVED
+import org.kabiri.android.usbterminal.ui.setting.SaveState.SAVED
 
+/**
+ * Enum class to represent the save state of the setting value.
+ * - DEFAULT: the default state of the setting value when it has not been changed
+ * - UNSAVED: the setting value has been changed but not saved
+ * - SAVED: the setting value has been saved
+ */
+private enum class SaveState {
+    DEFAULT, UNSAVED, SAVED
+}
+
+/**
+ * A composable that represents a setting item with a value that can be changed.
+ *
+ * @param currentValue the current value of the setting
+ * @param onNewValue a callback that will be called when a new value is set
+ */
 @Composable
 internal fun SettingValueItem(
     currentValue: Int,
     onNewValue: (Int) -> Unit
 ) {
     var inputValue by remember { mutableStateOf(currentValue.toString()) }
-    var showSaveIcon by remember { mutableStateOf(false) }
+    var saveState by remember { mutableStateOf(DEFAULT) }
 
     // keep baudRateInput updated with currentBaudRate
     LaunchedEffect(currentValue) {
@@ -35,25 +56,34 @@ internal fun SettingValueItem(
     OutlinedTextField(
         value = inputValue,
         onValueChange = { value: String ->
+            saveState = if (value.toIntOrNull() != currentValue) UNSAVED else DEFAULT
             inputValue = value
-            showSaveIcon = value.toIntOrNull() != currentValue
         },
-        label = { Text("Baud Rate") },
+        label = { Text(stringResource(id = R.string.settings_label_baud_rate)) },
         singleLine = true,
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
         trailingIcon = {
-            if (showSaveIcon) {
-                IconButton(onClick = {
-                    inputValue.toIntOrNull()?.let { baudRate ->
-                        onNewValue(baudRate)
-                        showSaveIcon = false
+            when (saveState) {
+                DEFAULT -> {}
+                UNSAVED -> {
+                    IconButton(onClick = {
+                        inputValue.toIntOrNull()?.let { baudRate ->
+                            onNewValue(baudRate)
+                            saveState = SAVED
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Check,
+                            contentDescription = stringResource(id = R.string.settings_bt_save)
+                        )
                     }
-                }) {
-                    Icon(
-                        imageVector = Icons.Outlined.Check,
-                        contentDescription = "Save"
+                }
+                SAVED -> {
+                    Text(
+                        text = stringResource(id = R.string.settings_label_saved),
+                        modifier = Modifier.padding(end = 16.dp)
                     )
                 }
             }
