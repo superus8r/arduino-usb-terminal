ba0cffab3ee0b5e06e54b145c41669a840cf3c49
Ali Kabiri <k4biri@icloud.com>
2025-05-07 13:18:41 +0200

    nitpick: remove unused import

--- app/src/main/java/org/kabiri/android/usbterminal/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/viewmodel/MainActivityViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
