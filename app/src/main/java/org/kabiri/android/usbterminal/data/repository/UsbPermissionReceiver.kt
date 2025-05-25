957a1401a2371f4d0851217a5096102b4bb2436e
Ali Kabiri <k4biri@icloud.com>
2025-05-25 09:39:04 +0200

    fix: unregister receiver once the permission result is received

--- app/src/main/java/org/kabiri/android/usbterminal/data/repository/UsbPermissionReceiver.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/data/repository/UsbPermissionReceiver.kt
@@ -7,12 +7,15 @@ import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import org.kabiri.android.usbterminal.Constants
 
-internal class UsbPermissionReceiver(private val repository: UsbRepository) : BroadcastReceiver() {
+internal class UsbPermissionReceiver(
+    private val repository: IUsbRepository,
+) : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == Constants.ACTION_USB_PERMISSION) {
             val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
             val granted = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)
             repository.onPermissionResult(device, granted)
+            context?.unregisterReceiver(this)
         }
     }
 }
