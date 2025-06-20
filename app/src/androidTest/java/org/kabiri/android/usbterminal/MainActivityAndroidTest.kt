895eeedead6bbaaf280db41ccc94c39266d2ab7c
Ali Kabiri <k4biri@icloud.com>
2025-06-20 07:20:53 +0200

    tests: make sure action items are accessible to allow testing on headless emulator e.g. on the CI

--- app/src/androidTest/java/org/kabiri/android/usbterminal/MainActivityAndroidTest.kt
+++ app/src/androidTest/java/org/kabiri/android/usbterminal/MainActivityAndroidTest.kt
@@ -1,12 +1,15 @@
 package org.kabiri.android.usbterminal
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,11 +21,28 @@ internal class MainActivityAndroidTest {
     @get:Rule
     var rule = activityScenarioRule<MainActivity>()
 
+    private fun ensureMenuIsAccessible(menuItemId: Int) {
+        try {
+            // Try to find the menu item first
+            onView(withId(menuItemId)).check(matches(isDisplayed()))
+        } catch (e: NoMatchingViewException) {
+            // If not found then open the overflow menu
+            openActionBarOverflowOrOptionsMenu(
+                InstrumentationRegistry.getInstrumentation().targetContext
+            )
+        }
+    }
+
     @Test
     fun checkUiViewsAreDisplayed() {
         onView(withId(R.id.tvOutput)).check(matches(isDisplayed()))
         onView(withId(R.id.btEnter)).check(matches(isDisplayed()))
         onView(withId(R.id.etInput)).check(matches(isDisplayed()))
+
+        // Ensure action items are accessible, either in the toolbar or via overflow
+        ensureMenuIsAccessible(R.id.actionSettings)
+        ensureMenuIsAccessible(R.id.actionConnect)
+        ensureMenuIsAccessible(R.id.actionDisconnect)
 
         // Check menu items are displayed
         onView(withId(R.id.actionSettings)).check(matches(isDisplayed()))
@@ -32,6 +52,8 @@ internal class MainActivityAndroidTest {
 
     @Test
     fun clickingSettingsOpensSettingsBottomSheet() {
+        // Ensure the action item is accisble, either in the toolbar or via overflow
+        ensureMenuIsAccessible(R.id.actionSettings)
         // Click the Settings menu item
         onView(withId(R.id.actionSettings)).perform(click())
         // Assert the bottom sheet content is displayed
