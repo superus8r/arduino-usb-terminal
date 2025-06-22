83aa59e0fdc8c842e3e37d096146ea69bda6ade9
Ali Kabiri <k4biri@icloud.com>
2025-06-22 17:33:50 +0200

    tests: make sure action buttons cannot be clicked or asserted in ui tests correctly regardless of being visible or hidden in an action menu

--- app/src/androidTest/java/org/kabiri/android/usbterminal/MainActivityAndroidTest.kt
+++ app/src/androidTest/java/org/kabiri/android/usbterminal/MainActivityAndroidTest.kt
@@ -3,13 +3,14 @@ package org.kabiri.android.usbterminal
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,15 +22,19 @@ internal class MainActivityAndroidTest {
     @get:Rule
     var rule = activityScenarioRule<MainActivity>()
 
-    private fun ensureMenuIsAccessible(menuItemId: Int) {
+    private fun ensureMenuIsAccessible(
+        menuItemId: Int,
+        onVisible: () -> Unit,
+        onOverflow: () -> Unit
+    ) {
         try {
             // Try to find the menu item first
             onView(withId(menuItemId)).check(matches(isDisplayed()))
+            onVisible()
         } catch (_: NoMatchingViewException) {
             // If not found then open the overflow menu
-            openActionBarOverflowOrOptionsMenu(
-                InstrumentationRegistry.getInstrumentation().targetContext
-            )
+            openActionBarOverflowOrOptionsMenu(getInstrumentation().targetContext)
+            onOverflow()
         }
     }
 
@@ -44,31 +49,70 @@ internal class MainActivityAndroidTest {
     }
 
     @Test
-    fun checkActionMenuItemsAreDisplayed() {
-        // arrange
-        // act
-        // Ensure action items are accessible, either in the toolbar or via overflow
-        ensureMenuIsAccessible(R.id.actionSettings)
-        ensureMenuIsAccessible(R.id.actionConnect)
-        ensureMenuIsAccessible(R.id.actionDisconnect)
+    fun checkActionMenuItemSettingsIsDisplayed() = ensureMenuIsAccessible(
+        menuItemId = R.id.actionSettings,
+        onVisible = {
 
-        // assert
-        // Check menu items are displayed
-        onView(withId(R.id.actionSettings)).check(matches(isDisplayed()))
-        onView(withId(R.id.actionConnect)).check(matches(isDisplayed()))
-        onView(withId(R.id.actionDisconnect)).check(matches(isDisplayed()))
-    }
+            // assert
+            onView(withId(R.id.actionSettings)).check(matches(isDisplayed()))
+        },
+        onOverflow = {
+
+            // assert
+            onView(withText(R.string.title_settings)).check(matches(isDisplayed()))
+        }
+    )
+
+    @Test
+    fun checkActionMenuItemConnectIsDisplayed() = ensureMenuIsAccessible(
+        menuItemId = R.id.actionSettings,
+        onVisible = {
+
+            // assert
+            onView(withId(R.id.actionConnect)).check(matches(isDisplayed()))
+        },
+        onOverflow = {
+
+            // assert
+            onView(withText(R.string.title_connect)).check(matches(isDisplayed()))
+        }
+    )
+
+    @Test
+    fun checkActionMenuItemDisconnectIsDisplayed() = ensureMenuIsAccessible(
+        menuItemId = R.id.actionSettings,
+        onVisible = {
+
+            // assert
+            onView(withId(R.id.actionDisconnect)).check(matches(isDisplayed()))
+        },
+        onOverflow = {
+
+            // assert
+            onView(withText(R.string.title_disconnect)).check(matches(isDisplayed()))
+        }
+    )
 
     @Test
     fun clickingSettingsOpensSettingsBottomSheet() {
         // arrange
-        // Ensure the action item is accisble, either in the toolbar or via overflow
-        ensureMenuIsAccessible(R.id.actionSettings)
+        ensureMenuIsAccessible(
+            menuItemId = R.id.actionSettings,
+            onVisible = {
 
-        // act
-        onView(withId(R.id.actionSettings)).perform(click())
+                // act
+                onView(withId(R.id.actionSettings)).perform(click())
 
-        // assert
-        onView(withId(R.id.composeViewSettingContent)).check(matches(isDisplayed()))
+                // assert
+                onView(withId(R.id.composeViewSettingContent)).check(matches(isDisplayed()))
+            },
+            onOverflow = {
+                // act
+                onView(withText(R.string.title_settings)).perform(click())
+
+                // assert
+                onView(withId(R.id.composeViewSettingContent)).check(matches(isDisplayed()))
+            }
+        )
     }
 }
