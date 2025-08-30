57bd271d5189069df6506667b36c7f3e2f5267d3
Ali Kabiri <k4biri@icloud.com>
2025-08-30 11:59:07 +0200

    feat: add auto scroll switch composable

---
+++ app/src/main/java/org/kabiri/android/usbterminal/ui/setting/SettingSwitchItem.kt
@@ -0,0 +1,30 @@
+package org.kabiri.android.usbterminal.ui.setting
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.kabiri.android.usbterminal.R
+
+@Composable
+internal fun SettingAutoScrollItem(
+    enabled: Boolean,
+    onToggle: (Boolean) -> Unit,
+) {
+    Row(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(text = stringResource(id = R.string.settings_label_auto_scroll))
+        Switch(checked = enabled, onCheckedChange = onToggle)
+    }
+}
