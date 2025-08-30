2b59b2eb7691a558bf207acbe1b2a7e4a83ab47e
Ali Kabiri <k4biri@icloud.com>
2025-08-30 14:14:32 +0200

    fix: improve auto-scroll text in settings item

--- app/src/main/java/org/kabiri/android/usbterminal/ui/setting/SettingSwitchItem.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/ui/setting/SettingSwitchItem.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -16,15 +18,20 @@ import org.kabiri.android.usbterminal.R
 internal fun SettingAutoScrollItem(
     enabled: Boolean,
     onToggle: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Row(
         modifier =
-            Modifier
+            modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp, vertical = 8.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Text(text = stringResource(id = R.string.settings_label_auto_scroll))
+        Text(
+            text = stringResource(id = R.string.settings_label_auto_scroll),
+            color = MaterialTheme.colorScheme.onPrimary,
+        )
         Switch(checked = enabled, onCheckedChange = onToggle)
     }
 }
