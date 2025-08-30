6bb57000738fd3906e8a3990f2a6ebf83d35f824
Ali Kabiri <k4biri@icloud.com>
2025-08-30 13:08:04 +0200

    feat: add autoScroll preference to UserSettingPreferences data class

--- app/src/main/java/org/kabiri/android/usbterminal/model/UserSettingPreferences.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/model/UserSettingPreferences.kt
@@ -3,5 +3,6 @@ package org.kabiri.android.usbterminal.model
 internal const val defaultBaudRate: Int = 9600
 
 data class UserSettingPreferences(
-    val baudRate: Int = defaultBaudRate // Arduino default
+    val baudRate: Int = defaultBaudRate, // Arduino default
+    val autoScroll: Boolean = true,
 )
