c19aaf7c3b6ce9c0b47b80321204f3f0e736c444
Ali Kabiri <k4biri@icloud.com>
2025-08-31 15:01:47 +0200

    refactor: reformat ArduinoRepository for readability; no changes

--- app/src/main/java/org/kabiri/android/usbterminal/data/repository/ArduinoRepository.kt
+++ app/src/main/java/org/kabiri/android/usbterminal/data/repository/ArduinoRepository.kt
@@ -32,165 +32,175 @@ interface IArduinoRepository {
     val messageFlow: Flow<String>
     val infoMessageFlow: Flow<String>
     val errorMessageFlow: Flow<String>
+
     fun disconnect()
+
     fun openDeviceAndPort(device: UsbDevice)
+
     fun serialWrite(command: String): Boolean
 }
 
 internal class ArduinoRepository
-@Inject constructor(
-    private val context: Context,
-    private val arduinoSerialReceiver: ArduinoSerialReceiver,
-    private val getBaudRate: IGetCustomBaudRateUseCase,
-): IArduinoRepository {
+    @Inject
+    constructor(
+        private val context: Context,
+        private val arduinoSerialReceiver: ArduinoSerialReceiver,
+        private val getBaudRate: IGetCustomBaudRateUseCase,
+    ) : IArduinoRepository {
+        private var currentBaudRate = DEFAULT_BAUD_RATE // Default value
 
-    private var currentBaudRate = DEFAULT_BAUD_RATE // Default value
+        private val _messageFlow = MutableStateFlow("")
+        override val messageFlow: Flow<String>
+            get() =
+                _messageFlow
+                    .combine(arduinoSerialReceiver.liveOutput) { a, b -> a + b }
 
-    private val _messageFlow = MutableStateFlow("")
-    override val messageFlow: Flow<String>
-        get() = _messageFlow
-            .combine(arduinoSerialReceiver.liveOutput) { a, b -> a + b }
+        private val _infoMessageFlow = MutableStateFlow("")
+        override val infoMessageFlow: Flow<String>
+            get() =
+                _infoMessageFlow
+                    .combine(arduinoSerialReceiver.liveInfoOutput) { a, b -> a + b }
 
-    private val _infoMessageFlow = MutableStateFlow("")
-    override val infoMessageFlow: Flow<String>
-        get() = _infoMessageFlow
-            .combine(arduinoSerialReceiver.liveInfoOutput) { a, b -> a + b }
+        private val _errorMessageFlow = MutableStateFlow("")
+        override val errorMessageFlow: Flow<String>
+            get() =
+                _errorMessageFlow
+                    .combine(arduinoSerialReceiver.liveErrorOutput) { a, b -> a + b }
 
-    private val _errorMessageFlow = MutableStateFlow("")
-    override val errorMessageFlow: Flow<String>
-        get() = _errorMessageFlow
-            .combine(arduinoSerialReceiver.liveErrorOutput) { a, b -> a + b }
+        private var usbManager: UsbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
+        private lateinit var connection: UsbDeviceConnection
+        private lateinit var serialPort: UsbSerialDevice
 
-    private var usbManager: UsbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
-    private lateinit var connection: UsbDeviceConnection
-    private lateinit var serialPort: UsbSerialDevice
-
-    init {
-        observeBaudRate()
-    }
-
-    override fun disconnect() {
-        try {
-            if (::connection.isInitialized) connection.close()
-            _messageFlow.value = context.getString(R.string.helper_info_serial_connection_closed)
-        } catch (e: UninitializedPropertyAccessException) {
-            _errorMessageFlow.value =
-                context.getString(R.string.helper_error_connection_not_ready_to_close)
-
-            _errorMessageFlow.value = "${e.localizedMessage}"
-        } catch (e: Exception) {
-            _errorMessageFlow.value = context.getString(
-                    R.string.helper_error_connection_failed_to_close
-                )
-            _errorMessageFlow.value = "${e.localizedMessage}"
-        }
-    }
-
-    /**
-     * This method should be called after the permission is granted to access the Arduino via USB.
-     */
-    override fun openDeviceAndPort(device: UsbDevice) {
-        try {
-            // setup the device communication.
-            connection = usbManager.openDevice(device)
-            serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection)
-        } catch (e: IllegalStateException) {
-            Log.e(TAG, "${e.message}")
-            _errorMessageFlow.value =
-                context.getString(R.string.helper_error_connection_closed_unexpectedly)
-        } catch (e: NullPointerException) {
-            Log.e(TAG, "${e.message}")
-            _errorMessageFlow.value = context.getString(
-                R.string.helper_error_connection_failed_to_open)
-        } catch (e: Exception) {
-            Log.e(TAG, "${e.message}")
-            _errorMessageFlow.value =
-                context.getString(R.string.helper_error_connection_failed_to_open_unknown)
+        init {
+            observeBaudRate()
         }
 
-        if (::serialPort.isInitialized)
-            prepareSerialPort(serialPort)
-        else {
-            _infoMessageFlow.value = context.getString(R.string.helper_error_serial_port_is_null)
-            if (::connection.isInitialized) connection.close()
-        }
-    }
-
-    /**
-     * Prepare the serial port to interact with Arduino
-     * and register the read callback to read the serial messages from Arduino.
-     */
-    private fun prepareSerialPort(serialPort: UsbSerialDevice) {
-        serialPort.let {
-            if (it.open()) {
-                // init the serial port and set connection params.
-                Log.d(TAG, "current baud rate = $currentBaudRate")
-                it.setBaudRate(currentBaudRate)
-                it.setDataBits(UsbSerialInterface.DATA_BITS_8)
-                it.setStopBits(UsbSerialInterface.STOP_BITS_1)
-                it.setParity(UsbSerialInterface.PARITY_NONE)
-                it.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
-                it.read(arduinoSerialReceiver) // messages will be received from the receiver.
-                _infoMessageFlow.value =
-                    context.getString(R.string.helper_info_serial_connection_opened)
-            } else { // serial port not opened.
+        override fun disconnect() {
+            try {
+                if (::connection.isInitialized) connection.close()
+                _messageFlow.value = context.getString(R.string.helper_info_serial_connection_closed)
+            } catch (e: UninitializedPropertyAccessException) {
                 _errorMessageFlow.value =
-                    context.getString(R.string.helper_error_serial_connection_not_opened)
+                    context.getString(R.string.helper_error_connection_not_ready_to_close)
+
+                _errorMessageFlow.value = "${e.localizedMessage}"
+            } catch (e: Exception) {
+                _errorMessageFlow.value =
+                    context.getString(R.string.helper_error_connection_failed_to_close)
+                _errorMessageFlow.value = "${e.localizedMessage}"
             }
         }
-    }
 
-    /**
-     * Send a serial message to the connected Arduino.
-     */
-    override fun serialWrite(command: String): Boolean {
-        return try {
-            if (::serialPort.isInitialized && command.isNotBlank()) {
-                serialPort.write(command.toByteArray())
-                true
+        /**
+         * This method should be called after the permission is granted to access the Arduino via USB.
+         */
+        override fun openDeviceAndPort(device: UsbDevice) {
+            try {
+                // setup the device communication.
+                connection = usbManager.openDevice(device)
+                serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection)
+            } catch (e: IllegalStateException) {
+                Log.e(TAG, "${e.message}")
+                _errorMessageFlow.value =
+                    context.getString(R.string.helper_error_connection_closed_unexpectedly)
+            } catch (e: NullPointerException) {
+                Log.e(TAG, "${e.message}")
+                _errorMessageFlow.value =
+                    context.getString(R.string.helper_error_connection_failed_to_open)
+            } catch (e: Exception) {
+                Log.e(TAG, "${e.message}")
+                _errorMessageFlow.value =
+                    context.getString(R.string.helper_error_connection_failed_to_open_unknown)
+            }
+
+            if (::serialPort.isInitialized) {
+                prepareSerialPort(serialPort)
             } else {
-                _errorMessageFlow.value =
-                    context.getString(R.string.helper_error_serial_port_is_null)
-                false
+                _infoMessageFlow.value = context.getString(R.string.helper_error_serial_port_is_null)
+                if (::connection.isInitialized) connection.close()
             }
-        } catch (e: Exception) {
-            _errorMessageFlow.value = context.getString(R.string.helper_error_write_problem) +
-                    " \n${e.localizedMessage}"
-            Log.e(TAG, "$e")
-            false
         }
-    }
 
-    /**
-     * Listen for changes in baudRate set by the user
-     */
-    private fun observeBaudRate() {
-        CoroutineScope(Dispatchers.IO).launch {
-            getBaudRate().collect { baudRate ->
-                currentBaudRate = baudRate
-                _infoMessageFlow.value = String.format(
-                    context.getString(R.string.helper_info_baud_rate_applying), baudRate)
-                if (::serialPort.isInitialized) {
-                    updateSerialPortBaudRate(baudRate)
-                    _infoMessageFlow.value = String.format(
-                        context.getString(R.string.helper_info_baud_rate_applied), baudRate)
-                } else {
+        /**
+         * Prepare the serial port to interact with Arduino
+         * and register the read callback to read the serial messages from Arduino.
+         */
+        private fun prepareSerialPort(serialPort: UsbSerialDevice) {
+            serialPort.let {
+                if (it.open()) {
+                    // init the serial port and set connection params.
+                    Log.d(TAG, "current baud rate = $currentBaudRate")
+                    it.setBaudRate(currentBaudRate)
+                    it.setDataBits(UsbSerialInterface.DATA_BITS_8)
+                    it.setStopBits(UsbSerialInterface.STOP_BITS_1)
+                    it.setParity(UsbSerialInterface.PARITY_NONE)
+                    it.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
+                    it.read(arduinoSerialReceiver) // messages will be received from the receiver.
+                    _infoMessageFlow.value =
+                        context.getString(R.string.helper_info_serial_connection_opened)
+                } else { // serial port not opened.
                     _errorMessageFlow.value =
-                        context.getString(R.string.helper_error_baud_rate_failed_to_apply_no_connection)
+                        context.getString(R.string.helper_error_serial_connection_not_opened)
                 }
             }
         }
-    }
 
-    /**
-     * Update value of current baud rate to new one from the user
-     */
-    private fun updateSerialPortBaudRate(baudRate: Int) {
-        try {
-            serialPort.setBaudRate(baudRate)
-        } catch (e: Exception) {
-            _errorMessageFlow.value = context.getString(R.string.helper_error_applying_baud_rate) +
+        /**
+         * Send a serial message to the connected Arduino.
+         */
+        override fun serialWrite(command: String): Boolean =
+            try {
+                if (::serialPort.isInitialized && command.isNotBlank()) {
+                    serialPort.write(command.toByteArray())
+                    true
+                } else {
+                    _errorMessageFlow.value =
+                        context.getString(R.string.helper_error_serial_port_is_null)
+                    false
+                }
+            } catch (e: Exception) {
+                _errorMessageFlow.value = context.getString(R.string.helper_error_write_problem) +
                     " \n${e.localizedMessage}"
+                Log.e(TAG, "$e")
+                false
+            }
+
+        /**
+         * Listen for changes in baudRate set by the user
+         */
+        private fun observeBaudRate() {
+            CoroutineScope(Dispatchers.IO).launch {
+                getBaudRate().collect { baudRate ->
+                    currentBaudRate = baudRate
+                    _infoMessageFlow.value =
+                        String.format(
+                            context.getString(R.string.helper_info_baud_rate_applying),
+                            baudRate,
+                        )
+                    if (::serialPort.isInitialized) {
+                        updateSerialPortBaudRate(baudRate)
+                        _infoMessageFlow.value =
+                            String.format(
+                                context.getString(R.string.helper_info_baud_rate_applied),
+                                baudRate,
+                            )
+                    } else {
+                        _errorMessageFlow.value =
+                            context.getString(R.string.helper_error_baud_rate_failed_to_apply_no_connection)
+                    }
+                }
+            }
+        }
+
+        /**
+         * Update value of current baud rate to new one from the user
+         */
+        private fun updateSerialPortBaudRate(baudRate: Int) {
+            try {
+                serialPort.setBaudRate(baudRate)
+            } catch (e: Exception) {
+                _errorMessageFlow.value = context.getString(R.string.helper_error_applying_baud_rate) +
+                    " \n${e.localizedMessage}"
+            }
         }
     }
-}
