795c69b5f34bbc716e3d3459ce28023d50c40ea1
Ali Kabiri <k4biri@icloud.com>
2026-08-06 22:41:27 +0200

    chore: upgrade project to Java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,12 +19,12 @@ repositories {
 android {
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlin {
-        jvmToolchain(17)
+        jvmToolchain(21)
     }
 
     buildFeatures {
