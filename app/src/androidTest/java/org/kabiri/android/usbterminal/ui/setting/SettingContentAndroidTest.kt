1e8eeff4a181edb9322e9152918bb0fb1bfb8d4e
Ali Kabiri <k4biri@icloud.com>
2026-08-22 08:24:48 +0200

    refactor(tests): update createAndroidComposeRule to v2 in instrumented tests

--- app/src/androidTest/java/org/kabiri/android/usbterminal/ui/setting/SettingContentAndroidTest.kt
+++ app/src/androidTest/java/org/kabiri/android/usbterminal/ui/setting/SettingContentAndroidTest.kt
@@ -2,7 +2,7 @@ package org.kabiri.android.usbterminal.ui.setting
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.flow.flowOf
